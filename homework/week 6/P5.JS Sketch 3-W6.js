fc8012313fbdb749618b405df7ba14fc101a310e
Olivia Hu <huo282@newschool.edu>
2021-05-01 23:12:37 -0400

    hw upload

--- homework/week 6/P5.JS Sketch 3-W6.js
+++ homework/week 6/P5.JS Sketch 3-W6.js
@@ -1,14 +1,16 @@
-//values for different stroke weights
+//constant variable for radius of circles
 const r = 5;
 function setup(){
 	createCanvas(685, 400);
 	noStroke();
 	noLoop();
 }
+//used a loop to create a pattern
 function draw(){
   background("white");
       for (let i = 0; i < width; i += 10) {
       for (let j = 0; j < height; j += 10) {
+//applied random variable for color of circles
       randFillR = random(255);
       randFillG = random(255);
       randFillB = random(255);
