ce8c6f9cd3413f15c97c6eb73ed02723b084b9b2
Olivia Hu <huo282@newschool.edu>
2021-05-01 23:03:33 -0400

    hw upload

---
+++ homework/week 6/P5.JS Sketch 1-W6.js
@@ -0,0 +1,20 @@
+const r = 5; 
+function setup() {
+	createCanvas(500, 400);
+	noStroke();
+	noLoop();
+}
+
+function draw() {
+  background("white");
+		for (let i = 0; i < width; i += 10) {
+	    for (let j = 0; j < height; j += 10) {
+	    randFillR = random(255);
+	    randFillG = random(255);
+	    randFillB = random(255);
+	    fill(randFillR, randFillG, randFillB);
+	    circle(i, j, r);
+    }
+  }
+}
+
---
+++ homework/week 6/P5.JS Sketch 3-W6.js
@@ -0,0 +1,19 @@
+//values for different stroke weights
+const r = 5;
+function setup(){
+	createCanvas(685, 400);
+	noStroke();
+	noLoop();
+}
+function draw(){
+  background("white");
+      for (let i = 0; i < width; i += 10) {
+      for (let j = 0; j < height; j += 10) {
+      randFillR = random(255);
+      randFillG = random(255);
+      randFillB = random(255);
+      fill(randFillR, randFillG, randFillB);
+      circle(i, j, r);
+    }
+  }
+}
