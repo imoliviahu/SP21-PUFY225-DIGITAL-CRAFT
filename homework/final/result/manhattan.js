b7580c2593be71ee60c4e4d6d744d39180feb6d3
Olivia Hu <huo282@newschool.edu>
2021-05-10 00:12:19 -0400

    final upload

--- homework/final/result/manhattan.js
+++ homework/final/result/manhattan.js
@@ -1,16 +1,16 @@
-//constant variable for radius of circles
+//constant variable for width of each square
 const w = 5;
 function setup() {
 	createCanvas(685, 400);
 	noStroke();
 	noLoop();
 }
-//used a loop to create a pattern
+//used a for loop to create a pattern
 function draw() {
   background("white");
       for (let i = 0; i < width; i += w*2) {
       for (let j = 0; j < height; j += w*2) {
-//applied random variable for color of circles
+//applied random variable for color of squares
       randFillR = random(255);
       randFillG = random(255);
       randFillB = random(255);
